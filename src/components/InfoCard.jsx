b17ba9f26e5b2efac21fce278502829b11b6ceda
Alberto Sánchez <albersanchez511@hotmail.com>
2024-11-05 09:52:17 +0100

    Eliminada sombas de tarjetas de información

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -11,7 +11,7 @@ function InfoCard({ title, contain, name, setSection = null, section }) {
 
   return (
     <div
-      className={`userInfo-card relative flex h-max w-full flex-wrap items-center overflow-hidden rounded-lg bg-customNavyBlue--dark p-4 text-xs shadow-md shadow-[#5548A3] ${section ? 'active' : null} ${!contain || title == 'Ubicación' ? null : 'cursor-pointer'}`}
+      className={`userInfo-card relative flex h-max w-full flex-wrap items-center overflow-hidden rounded-lg bg-customNavyBlue--dark p-4 text-xs ${section ? 'active' : null} ${!contain || title == 'Ubicación' ? null : 'cursor-pointer'}`}
       onClick={handleClick}
     >
       <span></span>
